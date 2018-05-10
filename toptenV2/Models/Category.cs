cb7d55dae378af6f9ac507d7efd95b77bc6f6ec9
Samuel <Samuel@SUPER-DUPER-BOO>
2018-05-10 15:43:02 -0300

    fixed crash, reinstaled listings, category and subcategory files

---
+++ toptenV2/Models/Category.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace toptenV2.Models
+{
+    public class Category
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+
+        public virtual ICollection<SubCategory> SubCategories { get; set; }
+    }
+}
---
+++ toptenV2/Models/Listing.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace toptenV2.Models
+{
+    public class Listing
+    {
+        public int ID { get; set; }
+        public int SubCategoryID { get; set; }
+        public string Name { get; set; }
+
+        [Display(Name = "Release Date")]
+        [DisplayFormat(DataFormatString = "{0:yyy-mmm-ddd}", ApplyFormatInEditMode = true)]
+        [DataType(DataType.Date)]
+        public DateTime ReleaseDate { get; set; }
+        public virtual SubCategory SubCategory { get; set; }
+
+    }
+}
